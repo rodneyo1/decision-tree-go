ab89d5bfbc0a66b30c4c29482230d3886ee457d6
moonyango <mosesadrian825@gmail.com>
2025-03-13 13:28:33 +0300

    feat: getting command line flags

---
+++ utils/flags.go
@@ -0,0 +1,15 @@
+package utils
+
+import "flag"
+
+var (
+	CommandPtr   = flag.String("c", "", "Specify the command")
+	InputPtr     = flag.String("i", "", "input csv file")
+	ColumnPtr    = flag.String("t", "", "name of the target column")
+	OutputPtr    = flag.String("o", "outputs/output.dt", "path to save trained dataset tree model")
+	ModelFilePtr = flag.String("m", "", "path to trained dataset for predictions")
+)
+
+func ParseFlag() {
+	flag.Parse()
+}
