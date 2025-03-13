606f2a3c80b110b9e1dc682c7748a9313a7ae5ef
anamivale <muhembelev@gmail.com>
2025-03-13 17:24:36 +0300

    Place data into rows and use them in batches of 100

--- utils/files.go
+++ utils/files.go
@@ -4,8 +4,10 @@ import (
 	"encoding/csv"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"slices"
 	"strconv"
 	"time"
 
@@ -34,7 +36,61 @@ func ContainsClassAttribute() bool {
 	columns, err := csvReader.Read()
 	models.Columns = columns
 
+	if err != nil {
+		fmt.Println("failed to read file content")
+		return false
+	}
+	if !slices.Contains(columns, *ColumnPtr) {
+		return false
+	}
+	var batch []map[string]interface{}
+
+		for {
+			row, err := csvReader.Read()
+
+			if err == io.EOF {
+				break
+			}
+			record := make(map[string]interface{})
+			for i, val := range row{
+				record[columns[i]] = parseValue(val)
+			}
+			batch = append(batch, record)
+
+			if len(batch) == 100 {
+				// train
+				batch = nil
+			}
+
+		}
+		fmt.Println(batch[0])
+
+		if len(batch) > 0 {
+			//train
+
+		}
+	
+
+
 	return false
 }
 
+func parseValue(value string) interface{} {
+	if intVal, err := strconv.Atoi(value); err == nil {
+		return intVal
+	}
 
+	if floatVal, err := strconv.ParseFloat(value, 64); err == nil {
+		return floatVal
+	}
+
+	if value == "true" || value == "false" {
+		return value == "true"
+	}
+
+	if dateVal, err := time.Parse("2006-01-02", value); err == nil {
+		return dateVal
+	}
+
+	return value
+}
