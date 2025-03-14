4bf64810567e242c3a8eb521c610fdd94b894554
vmuhembe <muhembelev@gmail.com>
2025-03-14 04:25:33 +0300

    feat: add constants to use to build the tree

---
+++ algorithm/builder.go
@@ -0,0 +1,8 @@
+package algorithm
+
+
+const (
+	MaxDepth       = 20    // Maximum tree depth
+	MinSamplesLeaf = 5     // Minimum samples required to form a leaf
+	MinInfoGain    = 0.001 // Minimum information gain required to split
+)
