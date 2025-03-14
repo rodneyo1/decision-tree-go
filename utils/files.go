ba2273c40688da110ff4de5448ac97d661d5e0ec
a-j-sheilla <sheillajuma001@gmail.com>
2025-03-14 03:04:06 +0300

    fix: add a more robust input file validation

--- utils/files.go
+++ utils/files.go
@@ -15,9 +15,21 @@ import (
 )
 
 func FileExtValidation() error {
-	if !(filepath.Ext(*InputPtr) != ".csv" || filepath.Ext(*InputPtr) == ".dt") {
-		fmt.Println("invalid file format. Either use .csv for input and .dt for output")
-		return errors.New("invalid file format")
+	inputExt := filepath.Ext(*InputPtr)
+	if *CommandPtr == "train" && inputExt != ".csv" {
+		return errors.New("input file must be a CSV for training")
+	}
+	if *CommandPtr == "predict" && inputExt != ".csv" {
+		return errors.New("input file must be a CSV for prediction")
+	}
+	if *CommandPtr == "train" && filepath.Ext(*OutputPtr) != ".dt" {
+		return errors.New("output file must have .dt extension for model")
+	}
+	if *CommandPtr == "predict" && filepath.Ext(*OutputPtr) != ".csv" {
+		return errors.New("output file must have .csv extension for predictions")
+	}
+	if *CommandPtr == "predict" && filepath.Ext(*ModelFilePtr) != ".dt" {
+		return errors.New("model file must have .dt extension")
 	}
 	return nil
 }
