62f7dab925e3d596de49abc022a8afbadcf880b1
vmuhembe <muhembelev@gmail.com>
2025-03-14 04:34:51 +0300

    feat: add getvaluekey function

--- models/data.go
+++ models/data.go
@@ -1,13 +1,13 @@
 package models
 
+import "fmt"
+
 var (
 	Records      []map[string]interface{}
 	Columns      []string
 	FeatureTypes map[string]string
 	TargetValues map[interface{}]int
 	TargetType   string
-
-
 )
 
 type TreeNode struct {
@@ -19,4 +19,11 @@ type TreeNode struct {
 	Children   map[string]*TreeNode `json:"children,omitempty"`   // For categorical features
 	Left       *TreeNode            `json:"left,omitempty"`       // For numerical features (< threshold)
 	Right      *TreeNode            `json:"right,omitempty"`      // For numerical features (>= threshold)
+}
+
+func GetValueKey(val interface{}) string {
+	if val == nil {
+		return "nil"
+	}
+	return fmt.Sprintf("%v", val)
 }
