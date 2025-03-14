0b95f7c18a3f0d4e63eec8541c61e96ab053db65
vmuhembe <muhembelev@gmail.com>
2025-03-14 04:45:14 +0300

    feat: CompareValues compares two values based on their types

--- models/data.go
+++ models/data.go
@@ -1,6 +1,9 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 var (
 	Records      []map[string]interface{}
@@ -37,4 +40,95 @@ type SplitCriteria struct {
 	SplitIndices map[string][]int // For categorical splits
 	LeftIndices  []int            // For numerical splits (<)
 	RightIndices []int            // For numerical splits (>=)
+}
+
+// CompareValues compares two values based on their types
+func CompareValues(a, b interface{}) int {
+	// Handle nil values
+	if a == nil && b == nil {
+		return 0
+	}
+	if a == nil {
+		return -1
+	}
+	if b == nil {
+		return 1
+	}
+
+	// Compare based on types
+	switch va := a.(type) {
+	case int:
+		switch vb := b.(type) {
+		case int:
+			if va < vb {
+				return -1
+			} else if va > vb {
+				return 1
+			}
+			return 0
+		case float64:
+			fa := float64(va)
+			if fa < vb {
+				return -1
+			} else if fa > vb {
+				return 1
+			}
+			return 0
+		}
+	case float64:
+		switch vb := b.(type) {
+		case int:
+			fb := float64(vb)
+			if va < fb {
+				return -1
+			} else if va > fb {
+				return 1
+			}
+			return 0
+		case float64:
+			if va < vb {
+				return -1
+			} else if va > vb {
+				return 1
+			}
+			return 0
+		}
+	case string:
+		if s, ok := b.(string); ok {
+			if va < s {
+				return -1
+			} else if va > s {
+				return 1
+			}
+			return 0
+		}
+	case bool:
+		if bo, ok := b.(bool); ok {
+			if !va && bo {
+				return -1
+			} else if va && !bo {
+				return 1
+			}
+			return 0
+		}
+	case time.Time:
+		if t, ok := b.(time.Time); ok {
+			if va.Before(t) {
+				return -1
+			} else if va.After(t) {
+				return 1
+			}
+			return 0
+		}
+	}
+
+	// Fallback: convert to strings and compare
+	aStr := fmt.Sprintf("%v", a)
+	bStr := fmt.Sprintf("%v", b)
+	if aStr < bStr {
+		return -1
+	} else if aStr > bStr {
+		return 1
+	}
+	return 0
 }
